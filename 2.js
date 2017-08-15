87cb8b4b66c04949c455562b88b0a10b523c58ea
vaibhavsalvi <vaibhav.v.salvi@gmail.com>
2017-08-15 14:09:08 +0530

    NodeTest

---
+++ 2.js
@@ -0,0 +1,12 @@
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+console.log(greeter);
+console.log(greeter.greet());
